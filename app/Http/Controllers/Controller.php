4452844c958c186ad5aae51c0222df840aec86f7
manjunathhalli <manjunathhalli629@gmail.com>
2022-04-10 11:49:06 +0530

    Added: Book Store Product Application added

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+/**
+ * @OA\Swagger(
+ *   basePath="",
+ *   schemes= {"http", "https"},
+ *   host = L5_SWAGGER_CONST_HOST,
+ *   @OA\Info(
+ *     version="1.0.0",
+ *     title="Swagger Integration with PHP Laravel",
+ *     description="Integrate Swagger in Laravel application",
+ *   @OA\Contact(
+ *          email="manjunathhalli@629.com"
+ *     ),
+ *   )
+ * )
+ */
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
